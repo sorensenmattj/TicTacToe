30a64d4546a135b33a0fef10427abf86ac72ac60
Matt Sorensen <sorensenmattj@gmail.com>
2018-11-08 17:38:52 +0000

    Empty board created with new board instance.

---
+++ TicTacToe/TicTacToe/Board.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class Board
+    {
+        public char[] State { get; set; }
+
+        public Board()
+        {
+            State = new char[] { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+        }
+    }
+}
---
+++ TicTacToe/UnitTests/BoardInteraction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicTacToe;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class BoardInteraction
+    {
+        private Board _board;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _board = new Board();
+        }
+
+        [TestMethod]
+        public void EmptyBoard()
+        {
+            char[] boardState = _board.State;
+            char[] expectedState =
+                new char[] { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+
+            for (int i = 0; i < expectedState.Length; i++)
+            {
+                Assert.AreEqual(expectedState[i], boardState[i]);
+            }
+        }
+    }
+}
