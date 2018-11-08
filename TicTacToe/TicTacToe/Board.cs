d5c9e703ae7c23cbaf51e62294c1b378d545b3d9
Matt Sorensen <sorensenmattj@gmail.com>
2018-11-08 19:08:08 +0000

    Added PlaceToken function to add a token to the board.

--- TicTacToe/TicTacToe/Board.cs
+++ TicTacToe/TicTacToe/Board.cs
@@ -10,9 +10,51 @@ namespace TicTacToe
     {
         public char[] State { get; set; }
 
+        public char[] ValidTokens => new char[] { 'X', 'O' };
+
+        /// <summary>
+        /// Initialise a new instance of the <see cref="Board"/> class.
+        /// </summary>
         public Board()
         {
             State = new char[] { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+        }
+
+        /// <summary>
+        /// Place a token on the board.
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown if the index is out of range of the board.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// Thrown if an invalid token is provided.
+        /// </exception>
+        public void PlaceToken(char token, int index)
+        {
+            if (index < 0 || index > State.Length - 1)
+            {
+                var message =
+                    $"Provided index must be between 0 and {State.Length - 1}.";
+                throw new ArgumentOutOfRangeException(nameof(index), message);
+            }
+
+            if (!ValidTokens.Contains(token))
+            {
+                var messageBuilder = new StringBuilder();
+
+                messageBuilder.Append($"Invalid token provided. Valid tokens are {ValidTokens[0]}");
+
+                for (int i = 1; i < ValidTokens.Length; i++)
+                {
+                    messageBuilder.Append($" and {ValidTokens[i]}");
+                }
+
+                messageBuilder.Append(".");
+
+                throw new ArgumentException(messageBuilder.ToString());
+            }
+
+            State[index] = token;
         }
     }
 }
--- TicTacToe/UnitTests/BoardInteraction.cs
+++ TicTacToe/UnitTests/BoardInteraction.cs
@@ -1,21 +1,21 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 using TicTacToe;
 
 namespace UnitTests
 {
-    [TestClass]
+    [TestFixture]
     public class BoardInteraction
     {
         private Board _board;
 
-        [TestInitialize]
+        [SetUp]
         public void SetUp()
         {
             _board = new Board();
         }
 
-        [TestMethod]
+        [TestCase]
         public void EmptyBoard()
         {
             char[] boardState = _board.State;
@@ -25,6 +25,86 @@ namespace UnitTests
             for (int i = 0; i < expectedState.Length; i++)
             {
                 Assert.AreEqual(expectedState[i], boardState[i]);
+            }
+        }
+
+        [TestCase('X', 0, new char[] { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' })]
+        [TestCase('X', 1, new char[] { ' ', 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ' })]
+        [TestCase('X', 2, new char[] { ' ', ' ', 'X', ' ', ' ', ' ', ' ', ' ', ' ' })]
+        [TestCase('X', 3, new char[] { ' ', ' ', ' ', 'X', ' ', ' ', ' ', ' ', ' ' })]
+        [TestCase('X', 4, new char[] { ' ', ' ', ' ', ' ', 'X', ' ', ' ', ' ', ' ' })]
+        [TestCase('X', 5, new char[] { ' ', ' ', ' ', ' ', ' ', 'X', ' ', ' ', ' ' })]
+        [TestCase('X', 6, new char[] { ' ', ' ', ' ', ' ', ' ', ' ', 'X', ' ', ' ' })]
+        [TestCase('X', 7, new char[] { ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X', ' ' })]
+        [TestCase('X', 8, new char[] { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' })]
+        [TestCase('O', 0, new char[] { 'O', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' })]
+        [TestCase('O', 1, new char[] { ' ', 'O', ' ', ' ', ' ', ' ', ' ', ' ', ' ' })]
+        [TestCase('O', 2, new char[] { ' ', ' ', 'O', ' ', ' ', ' ', ' ', ' ', ' ' })]
+        [TestCase('O', 3, new char[] { ' ', ' ', ' ', 'O', ' ', ' ', ' ', ' ', ' ' })]
+        [TestCase('O', 4, new char[] { ' ', ' ', ' ', ' ', 'O', ' ', ' ', ' ', ' ' })]
+        [TestCase('O', 5, new char[] { ' ', ' ', ' ', ' ', ' ', 'O', ' ', ' ', ' ' })]
+        [TestCase('O', 6, new char[] { ' ', ' ', ' ', ' ', ' ', ' ', 'O', ' ', ' ' })]
+        [TestCase('O', 7, new char[] { ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'O', ' ' })]
+        [TestCase('O', 8, new char[] { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'O' })]
+        public void PlaceToken(char token, int index, char[] expectedState)
+        {
+            char[] boardState;
+
+            _board.PlaceToken(token, index);
+
+            boardState = _board.State;
+
+            for (int i = 0; i < expectedState.Length; i++)
+            {
+                Assert.AreEqual(expectedState[i], boardState[i]);
+            }
+        }
+
+        [TestCase(-1)]
+        [TestCase(9)]
+        public void PlaceTokenAtInvalidIndex(int invalidIndex)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => _board.PlaceToken('X', invalidIndex));
+
+            try
+            {
+                _board.PlaceToken('X', invalidIndex);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                var messageParts = 
+                    ex.Message.Split(
+                        Environment.NewLine.ToCharArray(),
+                        StringSplitOptions.RemoveEmptyEntries);
+
+                var message = messageParts[0];
+                var parameterName = messageParts[1];
+
+                Assert.AreEqual(
+                    "Provided index must be between 0 and 8.",
+                    message);
+
+                Assert.AreEqual("Parameter name: index", parameterName);
+            }
+        }
+
+        [TestCase('A')]
+        [TestCase('Z')]
+        public void PlaceInvalidToken(char invalidToken)
+        {
+            Assert.Throws<ArgumentException>(
+                () => _board.PlaceToken(invalidToken, 0));
+
+            try
+            {
+                _board.PlaceToken(invalidToken, 0);
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.AreEqual(
+                    "Invalid token provided. Valid tokens are X and O.",
+                    ex.Message);
             }
         }
     }
